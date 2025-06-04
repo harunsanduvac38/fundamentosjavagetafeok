ad0f97769c8b7050c597b1d463c774beb3dcdb0a
harunsanduvac38 <harunsanduvac@gmail.com>
2025-06-04 09:09:05 +0200

    first commit

---
+++ Class01Primitivos.java
@@ -0,0 +1,49 @@
+
+
+public class Class01Primitivos {
+    public static void main(String[] args) {
+
+        // String saludo = "Hola mundo";
+
+        // java.util.Scanner uno;
+        // java.util.Scanner dos;
+
+        // Scanner tres ; 
+
+        // // QUIERO PEDIR AL USUARIO UN TEXTO
+
+        // Scanner teclado = new Scanner (System.in);
+        // System.out.println("Introduzca su nombre :");
+
+        // //ALMACENAMOS EL NOMBRE DEL USUARIO
+
+        // String nombre = teclado.nextLine();
+        // System.out.println("Bienvenido a Java " + nombre);
+
+
+
+        //CONVERSION AUTOMATICA
+        // byte menor = 15;
+        // int mayor = menor;
+
+
+        //CONVERSION ENTRE PRIMITIVOS
+        //int mayor = 15;
+        //byte menor = (byte) mayor;
+
+
+        //CONVERTIR STRING A PRIMITIVO
+        // String texto = "12";
+        // int variable = Integer.parseInt(texto);
+        
+
+
+        //GUARDAR UN PRIMITIVO EN STRING
+        // int numero = 15;
+        // String texto2 = String.valueOf(numero);
+        System.out.println("asdfasdf");
+        System.out.println("Fin de programa");
+
+    }
+    
+}
---
+++ Class02EjemploScanner.java
@@ -0,0 +1,17 @@
+// import java.util.Scanner;
+
+public class Class02EjemploScanner {
+    public static void main (String[] args) {
+
+        // Scanner teclado = new Scanner (System.in);
+        // System.out.println("Cual es tu nombre :");
+        // String nombre = teclado.nextLine();
+
+        System.out.println("sdfsadf");
+        // System.out.println("Cuantos años tienes :");
+        // int edad = teclado.nextInt();
+
+        // System.out.println("Bienvenido a Java " + nombre + " y tienes " + edad + " años");
+    }
+    
+}

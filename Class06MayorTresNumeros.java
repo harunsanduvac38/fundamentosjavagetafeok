7420ccff83f2ab078a8eb04251937938edef7274
harunsanduvac38 <harunsanduvac@gmail.com>
2025-06-04 11:24:52 +0200

    cambioclas06

--- Class06MayorTresNumeros.java
+++ Class06MayorTresNumeros.java
@@ -4,6 +4,8 @@ public class Class06MayorTresNumeros {
     public static void main (String[] args){
 
         int mayor = 0;
+        int intermedio = 0;
+        int menor = 0;
 
         Scanner teclado = new Scanner (System.in);
 
@@ -21,13 +23,25 @@ public class Class06MayorTresNumeros {
 
         if(numero1 > numero2 && numero1 > numero3){
             mayor = numero1;
+            if(numero2 > numero3){
+                menor = numero3;
+                intermedio = numero2;
+            }
         }else if(numero2 > numero1 && numero2 > numero3){
             mayor = numero2;
+            if(numero1 > numero3){
+                menor = numero3;
+                intermedio = numero1;
+            }
         }else {
             mayor = numero3;
+            if(numero1 > numero2){
+                menor = numero2;
+                intermedio = numero1;
+            }
         }
 
-        System.out.println("El mayor es : " + mayor);
+        System.out.println(" El mayor es : " + mayor + "\n" + " El menor es : " + menor + "\n" + " El intermedio es : " + intermedio);
     }
     
 }
